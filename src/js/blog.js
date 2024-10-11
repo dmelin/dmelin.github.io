e66972a355bedad9798d892c3d866d704ea805be
Daniel Melin <daniel.melin@gmail.com>
2024-10-11 20:49:34 +0200

    Added history push and other various things in JS

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,20 +1,48 @@
 const gitURL = "https://api.github.com/repos/dmelin/dmelin.github.io/contents/posts";
+var myPosts;
+
+var requestedPost = location.href.split("?post=")[1];
 
 $(document).ready(function () {
-	getPosts()
+    getPosts()
         .then((posts) => {
-            console.log(posts);
-            posts.posts.forEach((post, key) => {
-                const title = post.name.replace(".html", "").split("-").slice(3);
-                $("#post-list").append(`
-                    <li>
-                        <a href="#" data-post="${key}">
-                            <span class="title">${title}</span>
-                        </a>
-                    </li>
-                `);
+            myPosts = posts.posts;
+            myPosts.forEach((post, key) => {
+                const postMeta = post.name.replace(".html", "").split("-");
+                const title = postMeta.slice(3);
+                const date = postMeta.slice(0, 3).join("-");
+
+                var link = $("<a>");
+
+                var linkTitle = $("<span>");
+                linkTitle.text(title).addClass("title");
+
+                var linkDate = $("<span>");
+                linkDate.text(date).addClass("date");
+
+                link.append(linkTitle).append(linkDate)
+                    .attr("href", `?post=${date}-${title}`)
+                    .attr("post-id", key);
+                link.click(function (e) {
+                    e.preventDefault();
+                    const postId = $(this).attr("post-id");
+                    const postUrl = `?post=${date}-${title}`
+
+                    history.pushState({ postId: postId }, "", postUrl);
+
+                    getPost(postId);
+                });
+                const listItem = $("<li>");
+                listItem.append(link);
+                $("#post-list").append(listItem);
             })
-		})
+
+            if (requestedPost !== undefined) {
+                getPost(requestedPost);
+            } else {
+                getPost(myPosts.length - 1);
+            };
+        })
 		.catch((error) => {
 			console.log("error", error);
 		});
@@ -24,11 +52,11 @@ function getPosts() {
 	const postData = sessionStorage.getItem("posts");
     if (postData) {
         const posts = JSON.parse(postData);
-        console.log("Loaded posts from cache");
 
-        if (Date.now() / 1000 - posts.expires <= 0) {
+        let expired = posts.expires - Date.now() / 1000;
+        if (expired >= 1000) {
             return new Promise((resolve, reject) => {
-                resolve(resolve(posts));
+                resolve(posts);
             });
         } else {
 
@@ -51,12 +79,55 @@ function getPosts() {
     });
 }
 
+function getPost(id) {
+    if (isNaN(id)) id = getPostIdFromTitle(id);
+
+    if (id < 0) {
+        console.error("Post not found", id);
+        // handlePostNotFound();
+        return;
+    }
+
+    $("[post-id].active").removeClass("active");
+    $(`[post-id=${id}]`).addClass("active");
+
+    const postUrl = myPosts[id].download_url;
+
+    $.ajax({
+        url: postUrl,
+        success: function (data) {
+            var postContent = $(data);
+            var postMeta = $("<div>");
+            postMeta.addClass("meta")
+                .append(postContent.find("time"))
+            
+            postContent.find("h2").after(postMeta);
+            $("#post-content").html(postContent);
+        },
+        error: function (error) {
+            console.log("error", error);
+        },
+    });
+    console.log("post", id);
+}
+
 function storePosts(posts) {
 	sessionStorage.setItem(
 		"posts",
 		JSON.stringify({
-			expires: Date.now() / 1000 + 10000,
+			expires: Date.now() / 1000 + 60*60*1,
 			posts: posts,
 		})
 	);
 }
+
+function getPostIdFromTitle(title) {
+    return myPosts.findIndex((post) => post.name === `${title}.html`);
+}
+
+window.onpopstate = function (event) {
+	if (event.state && event.state.postId) {
+		var postId = event.state.postId;
+		getPost(postId);
+	}
+};
